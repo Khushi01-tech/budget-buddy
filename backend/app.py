ec0ed2cae94393f2a3410d199e26d9c9612a2eea
Khushi Shah <Khushi0101shah@gmail.com>
2026-04-02 21:28:59 -0600

    fix: bind to 0.0.0.0 for deployment

--- backend/app.py
+++ backend/app.py
@@ -186,5 +186,4 @@ def delete_budget(id):
     return jsonify({"message": "Budget deleted!"})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
-
+    app.run(host='0.0.0.0', port=5000, debug=False)
