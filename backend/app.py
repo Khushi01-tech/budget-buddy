12b406bfed011756b7754dda3deafaa9678c2bbc
Khushi Shah <Khushi0101shah@gmail.com>
2026-04-02 23:13:22 -0600

    feat: search and filter transactions

--- backend/app.py
+++ backend/app.py
@@ -186,4 +186,4 @@ def delete_budget(id):
     return jsonify({"message": "Budget deleted!"})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5001, debug=False)
